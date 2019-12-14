1839a399c9d7b06d1dd7c5352577e34df2389b57
Nevan Scott <nfscott@gmail.com>
2019-12-14 20:04:26 +0100

    Getting ready for more serverless functions

--- functions/test.js
+++
@@ -1,8 +0,0 @@
-const { NAME } = process.env
-
-exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: `No worries, ${ NAME }, all is working fine!`
-  })
-}
